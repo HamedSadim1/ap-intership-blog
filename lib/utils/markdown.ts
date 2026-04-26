96c3e18844b688f5f5b6b47e9d51d13667ba3fec
HamedSadim1 <hamid.sadim@outlook.com>
2026-04-26 19:39:28 +0200

    fix build

--- lib/utils/markdown.ts
+++ lib/utils/markdown.ts
@@ -4,7 +4,7 @@ import MarkdownIt from "markdown-it";
 import hljs from "highlight.js";
 // @ts-expect-error - Some packages don't have proper TS types
 import markdownItAnchor from "markdown-it-anchor";
-// @ts-expect-error
+// @ts-expect-error - markdown-it-container lacks TypeScript definitions
 import markdownItContainer from "markdown-it-container";
 
 /**
